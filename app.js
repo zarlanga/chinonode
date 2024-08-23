b5502138df1e4be679a71cb2577c60d3b4245a4d
zarlanga <robertozitto@gmail.com>
2024-08-23 17:46:42 -0300

    juegado, que se yo

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 let olabb = "olabb";
 console.log(olabb);
 let fs = require('fs');
-let dicc1 = require('./testdicc.json');
-console.log('lala ' + dicc1.length);
-for(let i =0 ; i < 5; i++ ) console.log(dicc1[i]);
 
+let dicc2 = require('./diccacortado.json');
+
+
+/*let dicc2 = dicc1.map(i => {return {
+    s: i.s,
+    t: i.t,
+    p: i.p,
+    e: i.e }
+})*/
+console.log(dicc2.find(e => e.s == "三"))
+console.log(dicc2.find(e => e.p == "san"))
+/*
+for(let i =0 ; i < 5; i++ ) { let p = Math.floor(Math.random() * dicc2.length)
+    console.log(p + ":" + JSON.stringify(dicc2[p]));
+}
+*/
 //let dicc = require('./diccionariochino.json')
 //let diccF = dicc.filter(i=> i.s.length == 1)
 //console.log(dicc.length)
 //console.log(diccF.length)
-/*fs.writeFile('testdicc.json', JSON.stringify(diccF), (err) => {
+/*fs.writeFile('diccAcortado.json', JSON.stringify(dicc2), (err) => {
     if (err) throw err;
     console.log('noesploto');
 })*/
