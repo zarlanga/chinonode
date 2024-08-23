f2060e0f2761f919820c993ca25c2615afa376f4
zarlanga <robertozitto@gmail.com>
2024-08-23 16:46:16 -0300

    filtrado diccionario

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 let olabb = "olabb";
-console.log(olabb)
-//const fs = require('fs');
-let dicc = require('./diccionariochino.json')
-console.log(dicc.length)
+console.log(olabb);
+let fs = require('fs');
+let dicc1 = require('./testdicc.json');
+console.log('lala ' + dicc1.length);
+for(let i =0 ; i < 5; i++ ) console.log(dicc1[i]);
+
+//let dicc = require('./diccionariochino.json')
+//let diccF = dicc.filter(i=> i.s.length == 1)
+//console.log(dicc.length)
+//console.log(diccF.length)
+/*fs.writeFile('testdicc.json', JSON.stringify(diccF), (err) => {
+    if (err) throw err;
+    console.log('noesploto');
+})*/
 
